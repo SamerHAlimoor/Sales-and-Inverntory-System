0120b449f48acbae83ac000adfa6864cba5bd06c
Samer Alimoor <sipar.alamour@gmail.com>
2022-11-26 10:15:21 +0200

    first commit

---
+++ resources/views/layouts/admin-layout.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+  <title>@yield('name')</title>
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="{{asset('assets/admin/plugins/fontawesome-free/css/all.min.css')}}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{asset('assets/admin/dist/css/adminlte.min.css')}}">
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="{{asset('assets/admin/fonts/SansPro/SansPro.min.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/admin/css/bootstrap_rtl-v4.2.1/bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/admin/css/bootstrap_rtl-v4.2.1/custom_rtl.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/admin/css/mycustomstyle.css')}}">
+  
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+  <!-- Navbar -->
+  @include('admin.includes.navbar')
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  
+    <!-- Sidebar -->
+    @include('admin.includes.sidebar')
+
+    <!-- /.sidebar -->
+ 
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+   @yield('breadcrumb')
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    @yield('content')
+
+   
+    <!-- /.content -->
+
+  </div>
+
+
+
+  <!-- /.content-wrapper -->
+
+  <!-- Control Sidebar -->
+    <!-- Control Sidebar -->
+    {{-- <aside class="control-sidebar control-sidebar-dark">
+      <!-- Control sidebar content goes here -->
+      <div class="p-3">
+        <h5>Title</h5>
+        <p>Sidebar content</p>
+      </div>
+    </aside> --}}
+    <!-- /.control-sidebar -->
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  @include('admin.includes.footer')
+
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="{{asset('assets/admin/plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('assets/admin/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('assets/admin/dist/js/adminlte.min.js')}}"></script>
+@yield('script')
+</body>
+</html>
